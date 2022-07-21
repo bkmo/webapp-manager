13a0a368a89426201468b1ddadae901e49cb1653
Your Name <you@example.com>
2022-07-20 21:29:00 -0600

    remove redirect cruft

--- firefox-extension/inject.js
+++
@@ -1,8 +0,0 @@
-const injectScript = () => {
-  const scriptElem = document.createElement('script')
-  scriptElem.src = browser.runtime.getURL('script.js')
-  document.head.appendChild(scriptElem)
-}
-
-if (document.readyState === 'interactive') injectScript()
-else window.addEventListener('load', injectScript)
