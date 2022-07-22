8aa73ea5072df718e0cd51b2df1ad9a88ec9ade8
bkmo <bkmo@example.com>
2022-07-21 21:50:07 -0600

    add bacgrount tabs to browser

--- usr/share/webapp-manager/firefox/profile/user.js
+++ usr/share/webapp-manager/firefox/profile/user.js
@@ -114,3 +114,32 @@ user_pref("network.captive-portal-service.enabled", false); // [FF52+]
  * [1] https://bugzilla.mozilla.org/1460537 ***/
 user_pref("network.connectivity-service.enabled", false);
 // user_pref("full-screen-api.ignore-widgets", true);
+// Disables "Recommend extensions as you browse" and "Recommend features as you browse"
+// https://support.mozilla.org/en-US/kb/recommendations-firefox
+user_pref(
+  "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons",
+  false
+);
+user_pref(
+  "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features",
+  false
+);
+
+// Disable bookmark bar by default
+user_pref("browser.toolbars.bookmarks.visibility", "never");
+// Manjaro specific workaround
+user_pref("distribution.Manjaro.bookmarksProcessed", true);
+
+// Support for custom browser: protocol for opening links in main browser
+user_pref("network.protocol-handler.expose.browser", false);
+user_pref("security.external_protocol_requires_permission", false);
+
+// Enable uBlock and extension for browser: protocol by default
+user_pref(
+  "extensions.webextensions.ExtensionStorageIDB.migrated.screenshots@mozilla.org",
+  true
+);
+user_pref(
+  "extensions.webextensions.ExtensionStorageIDB.migrated.uBlock0@raymondhill.net",
+  true
+);
